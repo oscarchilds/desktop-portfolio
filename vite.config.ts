53bb41e6a394c0a67af39a65ea58e5d33809cacd
Oscar Childs <oscarjchilds@gmail.com>
2025-07-21 20:50:28 +0100

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/desktop-site/',
+  base: '/desktop-portfolio/',
   plugins: [vue()],
   // plugins: [vue(), eslint()],
   resolve: {
